fa726457c01b4c47a4625b882985e37f19a8b4fd
hgari <hgari@hotmail.com>
2022-01-25 13:51:03 -0300

    base

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 //refresh page for new game
 resetBtn.addEventListener('click',() => location.reload());
+
+let choices=['Piedra','Pape','Tijera'];
+
+optionBtn.forEach(button => {
+    button.addEventListener('click',getPlayerChoice)
+}); 
+
+function computerPlay(){
+    return choices[Math.floor(Math.random().choices.length)] ;
+}
+
+
+function getPlayerChoice(e) {
+    let playerSelection= (e.target.id);
+    playerChoice = e.target.textContent;
+    //playRound(playerSelection, computerPlay());
+  }
+
